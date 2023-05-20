b921fbe2a15e2ccbf266c1827e9011f1161553a2
Ayush0202 <ayush.skg@gmail.com>
2023-05-20 23:58:54 +0530

    Intial Setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Editor from './components/Editor';
+import Navbar from './components/landingPage/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar /> 
+        <Editor />
+    </div>
+  );
+}
+
+export default App;
