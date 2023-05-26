5b0102379e401d7f0859be8f9f0bfa10cd66de86
Ayush0202 <ayush.skg@gmail.com>
2023-05-27 00:25:30 +0530

    protecting server api routes

--- server/routes/documentRoute.js
+++ server/routes/documentRoute.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const documentController = require('../controller/documentController')
+const requireAuth = require('../middleware/requireAuth')
+
+// require auth for all document routes
+router.use(requireAuth)
 
 router.get('/codes', documentController.getAllUserCodes) // dashboard
 router.post('/new', documentController.saveNewCode) // creating new document
